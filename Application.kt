967c3f54f7a238da1090d6412da460613a479877
77Maxim777 <chemsmozhem@gmail.com>
2025-07-30 21:01:54 +0300

    Create Application.kt

---
+++ Application.kt
@@ -0,0 +1,13 @@
+import android.app.Application
+
+class Application : Application() {
+    companion object {
+        lateinit var instance: Application
+        private set
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+}
